e18007a3119ff18de7092d6a49099e684fd2a19b
Asmaa Adel <adelasmaa333@gmail.com>
2023-07-17 09:40:36 -0400

    create handle env, hande env key functions

--- cmd/main.go
+++ cmd/main.go
@@ -1,2 +1,14 @@
 package main
-func main() {}
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/codescalersinternships/envserver-Asmaa/internal"
+)
+func main() {
+	http.HandleFunc("/env", internal.HandleEnv)
+	http.HandleFunc("/env/", internal.HandleEnvKey)
+
+	fmt.Println("Server started on port 8080")
+	http.ListenAndServe(":8080", nil)
+}
--- internal/server.go
+++ internal/server.go
@@ -1,1 +1,24 @@
 package internal
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func HandleEnv(w http.ResponseWriter, r *http.Request) {
+	for _, env := range os.Environ() {
+		fmt.Fprintln(w, env)
+	}
+}
+
+func HandleEnvKey(w http.ResponseWriter, r *http.Request) {
+	key := strings.TrimPrefix(r.URL.Path, "/env/")
+	value := os.Getenv(key)
+	if value != "" {
+		fmt.Fprintf(w, "%s=%s", key, value)
+	} else {
+		fmt.Fprintf(w, "Environment variable '%s' not found", key)
+	}
+}
